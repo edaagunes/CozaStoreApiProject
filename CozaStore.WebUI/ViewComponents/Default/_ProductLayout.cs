36fb34cefce8440e1abdecdca8fa68c2e4f9c327
edaagunes <edagunes00@hotmail.com>
2024-12-19 23:13:58 +0300

    Added products

--- CozaStore.WebUI/ViewComponents/Default/_ProductLayout.cs
+++ CozaStore.WebUI/ViewComponents/Default/_ProductLayout.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CozaStore.WebUI.Dtos.FeatureDtos;
+using CozaStore.WebUI.Dtos.ProductDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CozaStore.WebUI.ViewComponents.Default
 {
 	public class _ProductLayout:ViewComponent
 	{
-		public IViewComponentResult Invoke()
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _ProductLayout(IHttpClientFactory httpClientFactory)
 		{
+			_httpClientFactory = httpClientFactory;
+		}
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpClientFactory.CreateClient();
+
+			var responseMessage = await client.GetAsync("https://localhost:7043/api/Product");
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+				return View(values);
+			}
 			return View();
 		}
 	}

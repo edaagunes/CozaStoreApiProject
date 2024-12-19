089ad855f35315477b38d31fcc266f62a0704cc0
edaagunes <edagunes00@hotmail.com>
2024-12-19 22:23:28 +0300

    Added banner

---
+++ CozaStore.WebUI/Dtos/CategoryDtos/ResultCategoryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CozaStore.WebUI.Dtos.CategoryDtos
+{
+	public class ResultCategoryDto
+	{
+		public int CategoryId { get; set; }
+		public string CategoryName { get; set; }
+		public string Title { get; set; }
+		public string ImageUrl { get; set; }
+	}
+}
--- CozaStore.WebUI/ViewComponents/Default/_BannerLayout.cs
+++ CozaStore.WebUI/ViewComponents/Default/_BannerLayout.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CozaStore.WebUI.Dtos.CategoryDtos;
+using CozaStore.WebUI.Dtos.FeatureDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CozaStore.WebUI.ViewComponents.Default
 {
 	public class _BannerLayout : ViewComponent
 	{
-		public IViewComponentResult Invoke()
+		private readonly IHttpClientFactory _httpClientFactory;
+		public _BannerLayout(IHttpClientFactory httpClientFactory)
 		{
+			_httpClientFactory = httpClientFactory;
+		}
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpClientFactory.CreateClient();
+
+			var responseMessage = await client.GetAsync("https://localhost:7043/api/Category");
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+				return View(values);
+			}
 			return View();
 		}
 	}

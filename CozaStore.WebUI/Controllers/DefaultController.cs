ac8da1f70d70c6b7f8bb38d4b34bc5149669b3b5
edaagunes <edagunes00@hotmail.com>
2024-12-20 02:33:28 +0300

    Edited main page

--- CozaStore.WebUI/Controllers/DefaultController.cs
+++ CozaStore.WebUI/Controllers/DefaultController.cs
@@ -1,12 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CozaStore.WebUI.Dtos.ProductDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
 
 namespace CozaStore.WebUI.Controllers
 {
 	public class DefaultController : Controller
 	{
+		//private readonly IHttpClientFactory _httpClientFactory;
+
+		//public DefaultController(IHttpClientFactory httpClientFactory)
+		//{
+		//	_httpClientFactory = httpClientFactory;
+		//}
 		public IActionResult Index()
 		{
 			return View();
 		}
+
+		//[HttpGet]
+		//public async Task<IActionResult> GetProductDetails(int id)
+		//{
+		//	var client = _httpClientFactory.CreateClient();
+		//	var responseMessage = await client.GetAsync($"https://localhost:7043/api/Product/GetProduct?id={id}");
+
+		//	if (responseMessage.IsSuccessStatusCode)
+		//	{
+		//		var jsonData = await responseMessage.Content.ReadAsStringAsync();
+		//		var values = JsonConvert.DeserializeObject<ResultProductWithGetById>(jsonData);
+		//		return PartialView("GetProductDetails", values);
+		//	}
+
+		//	return View();
+		//}
+
 	}
 }
--- CozaStore.WebUI/ViewComponents/Default/_ModalLayout.cs
+++ CozaStore.WebUI/ViewComponents/Default/_ModalLayout.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CozaStore.WebUI.Dtos.ProductDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CozaStore.WebUI.ViewComponents.Default
 {
-	public class _ModalLayout:ViewComponent
+	public class _ModalLayout : ViewComponent
 	{
+
 		public IViewComponentResult Invoke()
 		{
 			return View();

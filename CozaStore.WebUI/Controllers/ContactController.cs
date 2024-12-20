564548e69a91861d17019c378156f8b4f4bdaf8c
edaagunes <edagunes00@hotmail.com>
2024-12-20 20:42:33 +0300

    Added contact page

---
+++ CozaStore.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,37 @@
+﻿using CozaStore.WebUI.Dtos.AboutDtos;
+using CozaStore.WebUI.Dtos.ContactDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CozaStore.WebUI.Controllers
+{
+	public class ContactController : Controller
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+		public ContactController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public async Task<IActionResult> Index(CreateContactDto createContactDto)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(createContactDto);
+			StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			var responseMessage = await client.PostAsync("https://localhost:7043/api/Contact",stringContent);
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index","Default");
+			}
+			return View();
+		}
+	}
+}
---
+++ CozaStore.WebUI/Dtos/ContactDtos/CreateContactDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CozaStore.WebUI.Dtos.ContactDtos
+{
+	public class CreateContactDto
+	{
+		public string SenderMail { get; set; }
+		public string Message { get; set; }
+	}
+}

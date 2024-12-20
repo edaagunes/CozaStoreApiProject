6b1e219a209f12464e238c0e8e317a31e8ccd5bc
edaagunes <edagunes00@hotmail.com>
2024-12-20 20:18:31 +0300

    Added about page

---
+++ CozaStore.WebUI/Dtos/AboutDtos/ResultAboutDto.cs
@@ -0,0 +1,14 @@
+﻿namespace CozaStore.WebUI.Dtos.AboutDtos
+{
+	public class ResultAboutDto
+	{
+		public int AboutId { get; set; }
+		public string CoverImage { get; set; }
+		public string OurStoryDetail { get; set; }
+		public string OurMissionDetail { get; set; }
+		public string QuoteDetail { get; set; }
+		public string QuoteAuthor { get; set; }
+		public string Image1 { get; set; }
+		public string Image2 { get; set; }
+	}
+}

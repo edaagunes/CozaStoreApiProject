04645e9529e64e1eb45acd540b0c9a775cad00ef
edaagunes <edagunes00@hotmail.com>
2024-12-21 20:52:29 +0300

    Added Category in admin

--- CozaStore.WebUI/Controllers/AdminCategoryController.cs
+++ CozaStore.WebUI/Controllers/AdminCategoryController.cs
@@ -1,12 +1,98 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CozaStore.WebUI.Dtos.CategoryDtos;
+using CozaStore.WebUI.Dtos.FeatureDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace CozaStore.WebUI.Controllers
 {
 	public class AdminCategoryController : Controller
 	{
-		public IActionResult Index()
+		private readonly IHttpClientFactory _httpClientFactory;
+		public AdminCategoryController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7043/api/Category");
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult CreateCategory()
 		{
 			return View();
 		}
+		[HttpPost]
+		public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(createCategoryDto);
+
+			StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			var responseMessage = await client.PostAsync("https://localhost:7043/api/Category", stringContent);
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+		public async Task<IActionResult> DeleteCategory(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.DeleteAsync("https://localhost:7043/api/Category?id=" + id);
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+		[HttpGet]
+		public async Task<IActionResult> UpdateCategory(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7043/api/Category/GetCategory?id=" + id);
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<UpdateCategoryDto>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+		{
+			var client = _httpClientFactory.CreateClient();
+
+			var jsonData = JsonConvert.SerializeObject(updateCategoryDto);
+
+			StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			var responseMessage = await client.PutAsync("https://localhost:7043/api/Category", stringContent);
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+
+			return View();
+		}
+
 	}
 }
---
+++ CozaStore.WebUI/Dtos/CategoryDtos/CreateCategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CozaStore.WebUI.Dtos.CategoryDtos
+{
+	public class CreateCategoryDto
+	{
+		public string CategoryName { get; set; }
+		public string Title { get; set; }
+		public string ImageUrl { get; set; }
+	}
+}

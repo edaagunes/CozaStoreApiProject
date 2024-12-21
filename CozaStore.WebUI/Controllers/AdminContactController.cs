1e09eb0e3280dec79db5c7893e2f0ea48933b6bf
edaagunes <edagunes00@hotmail.com>
2024-12-21 20:32:03 +0300

    Added Contact in admin

---
+++ CozaStore.WebUI/Controllers/AdminContactController.cs
@@ -0,0 +1,55 @@
+﻿using CozaStore.WebUI.Dtos.ContactDtos;
+using CozaStore.WebUI.Dtos.FeatureDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CozaStore.WebUI.Controllers
+{
+	public class AdminContactController : Controller
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+		public AdminContactController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7043/api/Contact");
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultContactDto>>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
+
+		public async Task<IActionResult> DeleteContact(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.DeleteAsync("https://localhost:7043/api/Contact?id=" + id);
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+		public async Task<IActionResult> DetailMessage(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7043/api/Contact/GetContact?id=" + id);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var value = JsonConvert.DeserializeObject<ResultContactDto>(jsonData);
+				return Json(value);
+			}
+			return View();
+		}
+	}
+}
---
+++ CozaStore.WebUI/Dtos/ContactDtos/ResultContactDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CozaStore.WebUI.Dtos.ContactDtos
+{
+	public class ResultContactDto
+	{
+		public int ContactId { get; set; }
+		public string SenderMail { get; set; }
+		public string Message { get; set; }
+	}
+}
